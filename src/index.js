1f95d9f1bb10e78026b204ce3554aae75aa053af
Elias Jr <elijah.junnior@outlook.com>
2022-06-09 09:02:22 -0300

    route to get statement created

--- src/index.js
+++ src/index.js
@@ -2,11 +2,31 @@ const express = require('express');
 const { v4: uuidv4 } = require('uuid');
 
 const app = express();
+
 const customers = [];
 
 app.use(express.json());
 
-app.post('/account', (req, res, nxt) => { 
+// Middleware
+function verifyIfExistsAccountCPF(req, res, nxt) { 
+
+  const { cpf } = req.headers;
+ 
+  const customer = customers.find(
+    customer => customer.cpf === cpf
+  );
+
+  if(!customer) { 
+    return res.status(400).json({error: 'Customer not found'});
+  }
+
+  req.customer = customer;
+
+  nxt();
+
+}
+
+app.post('/account', (req, res) => { 
 
   const { cpf, name } = req.body;
 
@@ -15,7 +35,7 @@ app.post('/account', (req, res, nxt) => {
   )
 
   if(customersAlreadyExists) { 
-    return res.status(400).send({ 
+    return res.status(400).json({ 
       error: 'Customers Already Exists'
     })
   }
@@ -28,6 +48,14 @@ app.post('/account', (req, res, nxt) => {
   })
 
   res.status(201).send();
+
+});
+
+app.get('/statement', verifyIfExistsAccountCPF, (req, res) => { 
+
+  const customer = req.customer;
+
+  return res.json(customer.statement);
 
 });
 
